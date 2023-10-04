37608fee4324a43327fee1b85da6fed41cfda618
claussen100 <claussenpatrick@gmail.com>
2023-10-04 00:19:08 -0300

    Tradução landing page e melhorias

--- src/lading-page/components/FeaturesSection.tsx
+++ src/lading-page/components/FeaturesSection.tsx
@@ -18,7 +18,7 @@ export default function FeaturesSection() {
   return (
     <section id="features" className="py-16">
       <div className="container mx-auto text-center">
-        <h2 className="text-3xl font-semibold mb-8">Key Features</h2>
+        <h2 className="text-3xl font-semibold mb-8">Principais funcionalidades</h2>
         <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {
             features.map((ft) =>
