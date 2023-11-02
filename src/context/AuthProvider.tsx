8b34ec7c6e4bc8ef1e8a52b0907998ac61894402
dvdgd <dvd.gd762@gmail.com>
2023-11-02 00:51:58 -0300

    fix [auth]: user able to enter in auth pages with expired token

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -25,16 +25,18 @@ export const AuthProvider = ({ children }: IChildrenProps) => {
   const authService = new AuthService();
 
   useEffect(() => {
+    setIsLoading(false);
+  }, [userSession]);
+
+  useEffect(() => {
     const userSessionStr = localStorage.getItem("@userSession");
     if (!userSessionStr) return;
 
     const userSession = JSON.parse(userSessionStr) as TUserSession;
 
     if (userSession?.session?.access_token) {
-      supabaseClient.auth.setSession(userSession.session);
-      setStateSession(userSession);
+      setUserSession(userSession);
     }
-    setIsLoading(false);
   }, []);
 
   const setUserSession = (userSession: TUserSession | undefined) => {
@@ -42,36 +44,41 @@ export const AuthProvider = ({ children }: IChildrenProps) => {
       return logout();
     }
 
-    supabaseClient.auth.setSession(userSession.session);
-    localStorage.setItem("@userSession", JSON.stringify(userSession));
-    setStateSession(userSession);
-    setIsLoading(false);
-  }
+    setIsLoading(true);
+    supabaseClient.auth.setSession(userSession.session).then(({ data }) => {
+      if (!data.session?.access_token) {
+        return setStateSession(undefined);
+      }
+
+      userSession.session = data.session;
+      localStorage.setItem("@userSession", JSON.stringify(userSession));
+      setStateSession(userSession);
+    });
+  };
 
   const logout = async () => {
     await supabaseClient.auth.signOut();
     localStorage.removeItem("@userSession");
     setStateSession(undefined);
-    setIsLoading(false);
-  }
+  };
 
   const login = async (props: TLoginBody) => {
     const session = await authService.login(props);
     setUserSession(session);
-    setIsLoading(false);
     return session;
-  }
+  };
 
   const signUp = async (props: TSignUpBody) => {
     const session = await authService.signUp(props);
     setUserSession(session);
-    setIsLoading(false);
     return session;
-  }
+  };
 
   return (
-    <CurrentUserContext.Provider value={{ userSession, login, logout, signUp, setUserSession, isLoading }}>
+    <CurrentUserContext.Provider
+      value={{ userSession, login, logout, signUp, setUserSession, isLoading }}
+    >
       {children}
     </CurrentUserContext.Provider>
-  )
-}
+  );
+};
