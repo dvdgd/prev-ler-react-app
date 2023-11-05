bfdcdcb8fe812367da1da72beab0663cd032839e
claussen100 <claussenpatrick@gmail.com>
2023-11-05 20:33:53 -0300

    fix [shared]: table colapse fixed

--- src/shared/components/Card/TableCard.tsx
+++ src/shared/components/Card/TableCard.tsx
@@ -1,12 +1,14 @@
-import { useMediaQuery } from "@chakra-ui/react";
+import { Box, useMediaQuery } from "@chakra-ui/react";
 import { BaseCard, BaseCardProps } from "./BaseCard";
 
 export function TableCard({ title, children }: Omit<BaseCardProps, "minW">) {
   const [isLargerThan940] = useMediaQuery("(min-width: 62em)");
 
   return (
-    <BaseCard title={title} maxW={isLargerThan940 ? "62em" : ""}>
-      {children}
-    </BaseCard>
+    <Box width="full">
+      <BaseCard title={title} maxW={isLargerThan940 ? "62em" : ""}>
+        {children}
+      </BaseCard>
+    </Box>
   )
 }
