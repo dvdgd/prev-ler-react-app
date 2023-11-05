c3c576163c88da0d919f73cbd2a267b30958679b
dvdgd <dvd.gd762@gmail.com>
2023-11-05 19:35:08 -0300

    misc [shared]: add minW in mini info card

--- src/shared/components/Card/MiniInfoCard.tsx
+++ src/shared/components/Card/MiniInfoCard.tsx
@@ -1,9 +1,10 @@
-import { Card, CardBody, Flex, FlexProps, Stat, StatLabel } from "@chakra-ui/react";
+import { Card, CardBody, CardProps, Flex, FlexProps, Stat, StatLabel } from "@chakra-ui/react";
 import { IChildrenProps } from "../../../@types/react-base-props";
 
 type MiniStatisticsCardProps = {
   title: string,
   icon: React.ReactNode,
+  minW?: CardProps["minW"]
 } & IChildrenProps
 
 function IconBox({ children, ...props }: IChildrenProps & FlexProps) {
@@ -19,11 +20,11 @@ function IconBox({ children, ...props }: IChildrenProps & FlexProps) {
   )
 }
 
-export function MiniInfoCard({ title, icon, children }: MiniStatisticsCardProps) {
+export function MiniInfoCard({ title, icon, children, minW }: MiniStatisticsCardProps) {
   return (
-    <Card minH='83px'>
+    <Card minH='83px' w={"full"} minW={minW ?? "200px"}>
       <CardBody>
-        <Flex flexDirection='row' align='center' justify='center' w='100%' columnGap={10}>
+        <Flex flexDirection='row' align='center' justify='center' columnGap={[4]}>
           <Stat me='auto'>
             <StatLabel
               fontSize='sm'
