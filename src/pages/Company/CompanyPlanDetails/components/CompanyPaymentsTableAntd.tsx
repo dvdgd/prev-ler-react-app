2256eb2a02a521d7e0919e55911b93fedb13f362
claussen100 <claussenpatrick@gmail.com>
2023-11-05 19:15:12 -0300

    misc [company]: column structure refactored

--- src/pages/Company/CompanyPlanDetails/components/CompanyPaymentsTableAntd.tsx
+++ src/pages/Company/CompanyPlanDetails/components/CompanyPaymentsTableAntd.tsx
@@ -1,54 +1,80 @@
-import { Box, HStack } from "@chakra-ui/react";
-import { Table } from "antd";
+import { Table, Tag } from "antd";
 import Column from "antd/es/table/Column";
 import { ColumnsType } from "antd/lib/table";
 import { useEffect, useState } from "react";
-import { TPayment } from "../../../../@types/payment";
+import { EPaymentStatus, TPayment } from "../../../../@types/payment";
 import { useShowToastErrorHandler } from "../../../../hooks/useShowToastErrorHandler";
-import { ViewIconAction } from "../../../../shared/components/ViewIconAction";
 import { PaymentService } from "../../../../shared/services/PaymentService";
+import { PaymentsTableOptions } from "./PaymentsTableOptions";
 
 const columns = (): ColumnsType<TPayment> => {
+
   return [
     {
-      title: "Identificador pagamento",
+      title: "# ID",
       dataIndex: "paymentId",
       key: "paymentId",
-      width: "5%",
+      width: "%55",
+      fixed: true,
+      render(_value, payment) {
+        const paymentId = payment.paymentId?.toString().padStart(2, "0");
+        return <>{paymentId}</>;
+      },
+
     },
     {
       title: "Plano",
       dataIndex: "title",
       key: "title",
+      render(_value, Title) {
+        const title = Title.subscription?.plan?.title
+        return <>{title}</>
+      }
     },
     {
-      title: "Data do pagamento",
+      title: "Data",
       dataIndex: "paymentDate",
       key: "paymentDate",
+      render(_value, Data) {
+        const date = new Date(Data.paymentDate!)
+        const formattedDate = date.toLocaleDateString('pt-BR')
+        return <>{formattedDate}</>
+      }
     },
     {
       title: "Valor",
       dataIndex: "value",
       key: "value",
+      render(_value, { value }) {
+        const price = `R$ ${value}`
+        return <>{price}</>
+      }
     },
     {
-      title: "Status pagamento",
+      title: "Status",
       dataIndex: "status",
       key: "status",
+      render(_value, { status }) {
+        const colorMapping = {
+          [EPaymentStatus.paid]: "green",
+          [EPaymentStatus.processing]: "yellow",
+          [EPaymentStatus.notPaid]: "red",
+        };
+
+        const color = colorMapping[status]
+        return <Tag color={color}>{status}</Tag>;
+      }
     },
     {
-      title: "Detalhes",
-      render: () => {
+      title: "Opções",
+      dataIndex: "",
+      key: "",
+      align: "center",
+      width: 150,
+      render: (_value, record) => {
         return (
           <>
-            <HStack alignContent={"space-between"} w="full" paddingX={10}>
-              <ViewIconAction
-                aria-label="Detalhes"
-                color={"black"}
-                cursor="pointer"
-                onClick={() => { }}
-              />
-            </HStack>
+            <PaymentsTableOptions payment={record} />
           </>
         );
       },
@@ -81,18 +107,17 @@ export const CompanyPaymentsTableAntd = () => {
       }
     }
     fetchPayments();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <Box m="20px" mt="100px">
-      <Table
-        loading={isLoading}
-        dataSource={payments}
-        columns={columns()}
-        rowKey={(data) => data.paymentId || 0}
-      >
-        <Column title="" />
-      </Table>
-    </Box>
+    <Table
+      loading={isLoading}
+      dataSource={payments}
+      columns={columns()}
+      rowKey={(data) => data.paymentId || 0}
+    >
+      <Column title="" />
+    </Table>
   );
 };
