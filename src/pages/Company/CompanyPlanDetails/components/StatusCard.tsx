925d301c7ca70eb48af5fc293cc4909237abbd09
dvdgd <dvd.gd762@gmail.com>
2023-11-10 21:18:11 -0300

    misc [company]: change text status in StatusCard

--- src/pages/Company/CompanyPlanDetails/components/StatusCard.tsx
+++ src/pages/Company/CompanyPlanDetails/components/StatusCard.tsx
@@ -1,15 +1,44 @@
 import { Text, VStack } from "@chakra-ui/react";
 import { BsCheckAll } from "react-icons/bs";
+import { ESubscriptionStatus } from "../../../../@types/subscription";
 import { useAuth } from "../../../../hooks/useCurrentUser";
 import { MiniInfoCard } from "../../../../shared/components/Card/MiniInfoCard";
 
+function BlankStatusCard() {
+  return (
+    <MiniInfoCard title={"Status da Assinatura"} icon={<BsCheckAll />}>
+      <Text>
+        --
+      </Text>
+    </MiniInfoCard>
+  );
+}
+
 export function StatusCard() {
   const { userSession } = useAuth();
+
+  const subscription = userSession?.user?.company?.subscriptions?.at(0);
+  if (!subscription) {
+    return <BlankStatusCard />;
+  }
+
+  if (!subscription.status) {
+    return <BlankStatusCard />;
+  }
+
+  const statusTextMap = {
+    [ESubscriptionStatus.active]: "Ativa",
+    [ESubscriptionStatus.canceled]: "Cancelada",
+    [ESubscriptionStatus.notPaid]: "Não paga",
+  }
+
+  const statusText = statusTextMap[subscription.status];
+
   return (
     <MiniInfoCard title={"Status da Assinatura"} icon={<BsCheckAll />}>
       <VStack w={"full"} align="flex-start">
         <Text>
-          {userSession?.user?.company?.subscriptions?.at(0)?.status}
+          {statusText}
         </Text>
       </VStack>
     </MiniInfoCard>
