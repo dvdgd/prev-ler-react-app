8276354e92c4a1f182fd3cefd987ee8c9b0c0e87
dvdgd <dvd.gd762@gmail.com>
2023-11-20 17:33:37 -0300

    feat [company]: add user modal

--- src/pages/Company/Users/components/CompanyUsersTableOptions.tsx
+++ src/pages/Company/Users/components/CompanyUsersTableOptions.tsx
@@ -1,7 +1,8 @@
-import { Center, HStack } from "@chakra-ui/react"
+import { Box, Button, Center, HStack, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, VStack, useDisclosure } from "@chakra-ui/react"
 import { DeleteIconAction } from "@shared/components/DeleteIconAction"
 import { EditIconAction } from "@shared/components/EditIconAction"
 import { ViewIconAction } from "@shared/components/ViewIconAction"
+import { getUserTypeText } from "@shared/functions/UserTypeMap"
 import { useNavigate } from "react-router-dom"
 import { TCompanyUser } from "types/company-user"
 
@@ -10,6 +11,7 @@ type CompanyUsersTableOptionsProps = {
 }
 
 export function CompanyUsersTableOptions({ userCompany }: CompanyUsersTableOptionsProps) {
+  const modal = useDisclosure();
   const navigate = useNavigate();
 
   const onEditClick = () => {
@@ -21,11 +23,46 @@ export function CompanyUsersTableOptions({ userCompany }: CompanyUsersTableOptio
     <>
       <Center>
         <HStack alignContent={"space-around"} paddingX={3}>
-          <ViewIconAction onClick={() => { console.log(userCompany) }} aria-label={"Visualizar"} />
+          <ViewIconAction onClick={modal.onOpen} aria-label={"Visualizar"} />
           <EditIconAction onClick={onEditClick} aria-label={"Editar"} />
           <DeleteIconAction deleteFn={() => { }} aria-label={"Excluir"} />
         </HStack>
       </Center>
+
+      <Modal isCentered size="xs" isOpen={modal.isOpen} onClose={modal.onClose}>
+        <ModalOverlay backdropFilter="blur(9px)" />
+        <ModalContent>
+          <ModalHeader>Usuário {userCompany.firstname}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <VStack spacing={6} alignItems={"flex-start"}>
+              <Box>
+                <Text fontWeight={"medium"}>Nome Completo</Text>
+                <Text>{userCompany.firstname} {userCompany.lastname}</Text>
+              </Box>
+              <Box>
+                <Text fontWeight={"medium"}>Email</Text>
+                <Text>{userCompany.email}</Text>
+              </Box>
+              <Box>
+                <Text fontWeight={"medium"}>Cargo</Text>
+                <Text>{userCompany.jobRole.jobName}</Text>
+              </Box>
+              <Box>
+                <Text fontWeight={"medium"}>Tipo</Text>
+                <Text>{getUserTypeText(userCompany.type)}</Text>
+              </Box>
+              <Box>
+                <Text fontWeight={"medium"}>Situação</Text>
+                <Text>{userCompany.isAuthorized ? "Autorizado" : "Não autorizado"}</Text>
+              </Box>
+            </VStack>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={modal.onClose}>Sair</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   )
 }
