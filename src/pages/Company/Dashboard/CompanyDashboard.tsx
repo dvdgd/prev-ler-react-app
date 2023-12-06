f556dd872f491b8ec97881a4c7875321b1205d84
dvdgd <dvd.gd762@gmail.com>
2023-12-06 01:03:52 -0300

    feat [company]: add company info in dashboard and buttons to navigate

--- src/pages/Company/Dashboard/CompanyDashboard.tsx
+++ src/pages/Company/Dashboard/CompanyDashboard.tsx
@@ -1,12 +1,62 @@
-import { Text } from "@chakra-ui/react";
+import { Button, Divider, HStack } from "@chakra-ui/react";
+import { CompanyCards } from "@pages/Admin/Company/Details/components/CompanyCards";
+import { FaBuildingUser } from "react-icons/fa6";
+import { RiOrganizationChart } from "react-icons/ri";
+import { TbBusinessplan } from "react-icons/tb";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../../hooks/useCurrentUser";
 
 export function CompanyDashboard() {
   const { userSession } = useAuth();
+  const navigate = useNavigate();
+
+  const userCompany = userSession?.user?.company
+  if (!userCompany) {
+    return <></>
+  }
+
+  Object.assign(userCompany, {
+    users: [
+      userSession.user?.profile,
+    ]
+  });
 
   return (
-    <Text flexWrap="wrap" maxWidth={500}>
-      FALAAAAAAAAA MEU NOBRE REPRESENTANTE {JSON.stringify(userSession?.user || "", null, 4) ?? ""}
-    </Text>
+    <>
+      <CompanyCards company={userCompany} />
+      <Divider />
+      <HStack w={'full'} spacing={8}>
+        <Button
+          w={"full"}
+          h={14}
+          size={"lg"}
+          colorScheme="brand"
+          leftIcon={<TbBusinessplan />}
+          onClick={() => navigate('plan-details')}
+        >
+          Gestão do plano
+        </Button>
+        <Button
+          w={"full"}
+          h={14}
+          size={"lg"}
+          colorScheme="brand"
+          leftIcon={<RiOrganizationChart />}
+          onClick={() => navigate('job-roles')}
+        >
+          Cargos
+        </Button>
+        <Button
+          w={"full"}
+          h={14}
+          size={"lg"}
+          colorScheme="brand"
+          leftIcon={<FaBuildingUser />}
+          onClick={() => navigate('users')}
+        >
+          Usuários
+        </Button>
+      </HStack>
+    </>
   );
 }
